cd6c05ec08cc937e875c92f8cfc1a4cc23b2a16e
Florian Matter <florianmatter@gmail.com>
2022-12-10 11:49:09 -0800

    cleaned up ye'kwana corpus

--- workflow/run_annotator.py
+++ workflow/run_annotator.py
@@ -4,7 +4,8 @@ import re
 from pathlib import Path
 import json
 
-lg_list = ["hix", "tri", "aka", "mak"]
+lg_list = ["hix", "tri", "aka", "yab"]
+# lg_list = ["yab"]
 TARGET = 1000
 LIMIT = 10
 
@@ -27,28 +28,46 @@ def print_record(rec):
         rec["Analyzed_Word"].split("\t"),
         rec["Gloss"].split("\t"),
         rec["Part_Of_Speech"].split("\t"),
+        [str(x+1) for x in range(0, len(rec["Analyzed_Word"].split("\t")))]
     )
     surf = re.sub(r"\s+", " ", rec["Primary_Text"])
     print(f"""({rec.name}) {surf}\n{gloss_string}\n‘{rec['Translated_Text']}’\n""")
 
 
 for lg in lg_list:
+    print(lg)
     ann_path = Path(f"data/{lg}_ann.csv")
     if ann_path.is_file():
         info = pd.read_csv(ann_path)
     else:
-        info = pd.DataFrame(columns=["ID", "Value", "Comment", "Syntactic_Role"])
+        info = pd.DataFrame(
+            columns=[
+                "Example_ID",
+                "Value",
+                "Comment",
+                "Pattern",
+                "Positions",
+                "Animacy",
+                "Role",
+                "Particle",
+            ]
+        )
 
     df = pd.read_csv(f"data/{lg}_data.csv")
-
-    df = df.merge(info, on="ID", how="outer").fillna("")
+    if "Comment" in df.columns:
+        df.drop(columns=["Comment"], inplace=True)
+    df = df.merge(info, left_on="ID", right_on="Example_ID", how="outer").fillna("")
     df = df.iloc[0:TARGET]
+
+
     df["Todo"] = df.apply(
         lambda x: (x["Pre_Screened"] != "y" and x["Value"] == ""), axis=1
     )
+
     eliminated = df[(df["Pre_Screened"] == "y") & (df["Value"] == "")]
     annotated = len(df[df["Value"] != ""])
     todo = len(df[df["Todo"]])
+    print(len(eliminated), annotated, todo)
     eliminated[["ID"]].to_csv(f"data/{lg}_elim.csv", index=False)
     eliminated = len(eliminated)
     assert eliminated + annotated + todo == TARGET
@@ -80,34 +99,29 @@ for lg in lg_list:
             answered += 1
             choices = [
                 "No",
-                "Yes!",
-                "Particle",
-                "Postposition",
-                "A nice 'NP'",
+                "Yes",
+                "Just a comment",
                 "It's complicated",
                 "I want out",
             ]
             answer = questionary.rawselect(
-                "Any discontinuous NP?", choices=choices
+                "Any pseudo-NP?", choices=choices
             ).ask()
             if answer == choices[0]:
-                new_info.append({"ID": rec["ID"], "Value": "n"})
+                new_info.append({"Example_ID": rec["ID"], "Value": "n"})
             elif answer == choices[1]:
-                new_info.append({"ID": rec["ID"], "Value": "y"})
+                pattern = questionary.text("Pattern?").ask()
+                positions = questionary.text("Positions?").ask()
+                role = questionary.text("Role?").ask()
+                animacy = questionary.rawselect("Animacy?", choices=["hum", "anim", "inan"]).ask()
+                new_info.append({"Example_ID": rec["ID"], "Value": "y", "Pattern": pattern, "Positions": positions, "Role": role, "Animacy": animacy})
             elif answer == choices[2]:
-                new_info.append({"ID": rec["ID"], "Value": "part"})
+                comment = questionary.text("Comment?").ask()
+                new_info.append({"Example_ID": rec["ID"], "Value": "n", "Comment": comment})
             elif answer == choices[3]:
-                new_info.append({"ID": rec["ID"], "Value": "posp"})
-            elif answer == choices[4]:
-                new_info.append({"ID": rec["ID"], "Value": "np"})
-            elif answer == choices[5]:
                 comment = questionary.text("What's the problem?").ask()
-                if " / " in comment:
-                    comment, value = comment.split(" / ")
-                else:
-                    value = "?"
-                new_info.append({"ID": rec["ID"], "Value": value, "Comment": comment})
-            elif answer == choices[6]:
+                new_info.append({"Example_ID": rec["ID"], "Value": "?", "Comment": comment})
+            elif answer == choices[4]:
                 break
             print(
                 "----------------------------------------------------------------------------"
